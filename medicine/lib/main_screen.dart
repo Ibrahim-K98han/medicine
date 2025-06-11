2148df234fe6af2e19669e78accee104cefd600b
unknown <ibrahimkhan.dev405@gmail.com>
2025-06-11 09:20:59 +0600

    home screen category item ui done

--- medicine/lib/main_screen.dart
+++ medicine/lib/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:medicine/pages/history_screen.dart';
+import 'package:medicine/pages/home_screen.dart';
+import 'package:medicine/pages/profile_screen.dart';
+import 'package:medicine/theme.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,8 +12,35 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectIndex = 0;
+  final _screenList = [HomeScreen(), HistoryScreen(), ProfileScreen()];
+  onTappedItem(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _screenList[_selectIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectIndex,
+        onTap: onTappedItem,
+        unselectedItemColor: grey35Color,
+      ),
+    );
   }
 }
---
+++ medicine/lib/widgets/card_category.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:medicine/theme.dart';
+
+class CardCategory extends StatelessWidget {
+  const CardCategory({super.key, this.imageCategory, this.nameCategory});
+
+  final String? imageCategory;
+  final String? nameCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(imageCategory.toString(), width: 65),
+        Text(
+          nameCategory.toString(),
+          style: mediumTextStyle.copyWith(fontSize: 10),
+        ),
+      ],
+    );
+  }
+}
